5b414039618441e2b0bad1c21e1d2530a9413b3b
ANRU-Corp <antoni@rucinski.pol.pl>
2017-11-05 13:35:33 +0100

    updates and fixes
    test: read from file

--- SudokuAG/src/AppManager.java
+++ SudokuAG/src/AppManager.java
@@ -1,6 +1,23 @@
-/**
- * Created by longman on 31.10.17.
- */
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class AppManager {
+    public static void main(String[] args) throws IOException {
+
+
+        List<Integer> list = new ArrayList<Integer>();
+        list = ModelManager.getBoardFromFile("/home/anru/Documents/PW/AlGen/src/sudoku.txt");
+        System.out.print(list.get(15));
+
+
+    }
+
+
+
+
 
 }
+
+
